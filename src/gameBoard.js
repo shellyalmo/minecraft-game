3120d7e6b8f3bd245317699c0d3972085f94c26f
shellyalmo <shellyalmo@gmail.com>
2023-02-02 17:50:23 +0200

    feat:  side menu tools, inventory div & reset btn

---
+++ src/gameBoard.js
@@ -0,0 +1,4 @@
+// all needs to be blue sky and then add layers
+const gameArray = [[]];
+
+/**side menu of tools, inventory and reset button */
--- src/main.js
+++
@@ -1,5 +0,0 @@
-/* move from instructions opening screen to the game screen */
-// const startGameBtn = document.querySelector("start-game-btn");
-// startGameBtn?.addEventListener("click", function (e) {
-//   loc;
-// });
