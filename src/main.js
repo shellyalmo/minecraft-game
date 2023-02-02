a09e171d04630cabb7e1cbe7fa7bd8fea4fd5089
shellyalmo <shellyalmo@gmail.com>
2023-02-02 16:52:25 +0200

    feat: add opening screen

---
+++ src/main.js
@@ -0,0 +1,5 @@
+/* move from instructions opening screen to the game screen */
+// const startGameBtn = document.querySelector("start-game-btn");
+// startGameBtn?.addEventListener("click", function (e) {
+//   loc;
+// });
