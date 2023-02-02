3e9265fadd618eca79fb25a7021f7470547307f1
shellyalmo <shellyalmo@gmail.com>
2023-02-02 19:19:13 +0200

    feat: add gameboard cells as divs with images

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -1,4 +1,70 @@
 // all needs to be blue sky and then add layers
-const gameArray = [[]];
+// 0= blue sky, 1=cloud, 2=wood,3=tree,4=soil,5=sand,6=stone
+const gameArray = [
+  //   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  //   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, , 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3],
+  [0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3],
+  [0, 0, 0, 4, 4, 4, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0],
+  [5, 5, 5, 4, 4, 4, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 2, 0, 0],
+  [5, 5, 5, 4, 4, 4, 2, 2, 2, 2, 2, 4, 4, 4, 4, 4, 5, 2, 5, 5],
+  [6, 6, 6, 4, 4, 4, 4, 4, 4, 5, 6, 6, 6, 6, 4, 4, 5, 5, 5, 6],
+  [6, 6, 6, 4, 4, 4, 4, 4, 4, 5, 6, 6, 6, 6, 4, 6, 6, 6, 6, 6],
 
-/**side menu of tools, inventory and reset button */
+  //   [5, 5, 4, 4, 0, 0, 6, 6, 6, 0],
+  //   [5, 5, 4, 4, 4, 5, 5, 6, 6, 0],
+  //   [0, 5, 4, 5, 0, 0, 6, 6, 0, 0],
+  //   [5, 5, 4, 4, 0, 0, 6, 6, 6, 0],
+  //   [5, 5, 4, 4, 4, 5, 5, 6, 6, 0],
+];
+
+const gameBoard = document.getElementById("cells-board");
+
+/**
+ * generate elements of the board with their css classes
+ */
+function createBoard() {
+  for (let i = 0; i < gameArray.length; i++) {
+    for (let j = 0; j < gameArray[i].length; j++) {
+      const cell = document.createElement("div");
+      gameBoard.appendChild(cell);
+
+      cell.classList.add("sky");
+      switch (gameArray[i][j]) {
+        case 1:
+          cell.classList.add("cloud");
+          //   gameBoard.appendChild(cell);
+          break;
+        case 2:
+          cell.classList.add("wood");
+          //   gameBoard.appendChild(cell);
+          break;
+        case 3:
+          cell.classList.add("tree");
+          //   gameBoard.appendChild(cell);
+          break;
+        case 4:
+          cell.classList.add("soil");
+          //   gameBoard.appendChild(cell);
+          break;
+        case 5:
+          cell.classList.add("sand");
+          //   gameBoard.appendChild(cell);
+          break;
+        case 6:
+          cell.classList.add("stone");
+          //   gameBoard.appendChild(cell);
+          break;
+      }
+    }
+  }
+}
+
+createBoard();
