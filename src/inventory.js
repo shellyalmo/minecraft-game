825f16b864112eb2575bc15d35dbe54c1f4a1ec0
shellyalmo <shellyalmo@gmail.com>
2023-02-02 22:09:40 +0200

    refactor

---
+++ src/inventory.js
@@ -0,0 +1,6 @@
+//array of class names
+//inventory div with img of the last added element
+//click inventory div --> going to function addCell()
+//addCell()
+
+//when i hcang eto blue - push to array- when put it back pop
