ef7e72a960652d34da7e2cb6c5a5ff3897991f54
shellyalmo <shellyalmo@gmail.com>
2023-02-02 22:29:59 +0200

    style opening screen

--- src/inventory.js
+++ src/inventory.js
@@ -1,6 +1,5 @@
 //array of class names
+// push to array when about to delete
 //inventory div with img of the last added element
 //click inventory div --> going to function addCell()
 //addCell()
-
-//when i hcang eto blue - push to array- when put it back pop
